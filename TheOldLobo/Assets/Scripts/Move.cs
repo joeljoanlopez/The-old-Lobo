f9397652fb8f0498c8d65933d90fdc480572fdb1
Joloharo <joeljoanlopez@gmail.com>
2023-04-17 12:14:23 +0200

    Dash implementado se puede toquetear en el editor

--- TheOldLobo/Assets/Scripts/Move.cs
+++ TheOldLobo/Assets/Scripts/Move.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,66 +9,70 @@ public class Move : MonoBehaviour
 {
     private Rigidbody2D body;
 
-    [SerializeField] private int spe = 0;
-    [SerializeField] private float dashingCooldown;
+    [SerializeField] private float spe = 0;
+    [SerializeField] float dashPower = 0;
+    [SerializeField] float dashTime = 0;
+    [SerializeField] private float dashingCooldown = 0;
+
+    private float horizontal;
+    private float vertical;
 
     private bool sprinting;
+
+    //Dashing handlers
     private bool canDash;
     private bool dashing;
-    private float dashTime;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        sprinting = false;
         canDash = true;
         dashing = false;
-        dashTime = 0.5f;
-        dashingCooldown = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get Dash
-        if (Input.GetKey(KeyCode.Space) && canDash)
-        {
-            StartCoroutine(Dash());
-        }
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
         if (dashing)
         {
             return;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (canDash && Input.GetKeyDown(KeyCode.Space))
         {
-            transform.Translate(new Vector3(-spe, 0, 0) * Time.deltaTime);
+            StartCoroutine(Dash());
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!dashing)
         {
-            transform.Translate(new Vector3(spe, 0, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(new Vector3(0, spe, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(new Vector3(0, -spe, 0) * Time.deltaTime);
+            body.velocity = MovVector();
         }
     }
 
-    IEnumerator Dash()
+    private Vector2 MovVector()
+    {
+        Vector2 mov = new Vector2(horizontal, vertical).normalized;
+        return mov * spe * Time.deltaTime;
+    }
+
+    private IEnumerator Dash()
     {
         canDash = false;
         dashing = true;
-        spe *= 5;
+        body.velocity = MovVector() * dashPower;
         yield return new WaitForSeconds(dashTime);
-        spe /= 5;
         dashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
     }
+
+
 }
