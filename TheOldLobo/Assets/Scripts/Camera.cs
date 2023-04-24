2bc2b851ecbc635605aa6a3915230ffc24d374f6
Yifan-AFK <yifan1647wang@gmail.com>
2023-04-24 15:07:26 +0200

    Camara 2

---
+++ TheOldLobo/Assets/Scripts/Camera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    public float cameraSpeed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    private void LateUpdate()
+    {
+        float posX = Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime * cameraSpeed);
+        float posY = Mathf.Lerp(transform.position.y, player.transform.position.y, Time.deltaTime * cameraSpeed);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
