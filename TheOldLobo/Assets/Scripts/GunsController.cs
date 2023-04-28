c46d511943c6ee2dfdd393877393bf7a4145c7f0
Joloharo <joeljoanlopez@gmail.com>
2023-04-28 15:47:04 +0200

    Pistolas rotan alrededor del personaje

---
+++ TheOldLobo/Assets/Scripts/GunsController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunsController : MonoBehaviour
+{
+    private Vector2 _pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = GetRotation();
+    }
+
+    private Quaternion GetRotation()
+    {
+        //Get the Screen positions of the object and the mouse
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = GetAngleFromPoints(mouseOnScreen, positionOnScreen);
+
+        //return the rotation
+        return Quaternion.Euler(new Vector3(0f, 0f, angle));
+
+        //var fromVec = ;
+        //var toVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //return Quaternion.FromToRotation(fromVec, toVec);
+    }
+
+    float GetAngleFromPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+
+
+}
