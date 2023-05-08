1eba7a025d6092fd9971171dc669e1c9f97f64f5
VoltorH <darkkrasher@gmail.com>
2023-05-08 18:18:19 +0200

    Transicion de nivel

---
+++ TheOldLobo/Assets/Scripts/ExplosiveEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveEnemy 
+{
+    [SerializeField]
+    GameObject Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeOne();
+    }
+
+    // Update is called once per frame
+    public void MakeOne()
+    {
+        
+    }
+}
