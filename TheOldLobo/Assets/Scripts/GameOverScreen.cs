e33ecb879f1181400bf1a38c5fd88ea5a5bf042b
Yifan-AFK <yifan1647wang@gmail.com>
2023-05-10 01:33:22 +0200

    player quieto cuando sale gameover

--- TheOldLobo/Assets/Scripts/GameOverScreen.cs
+++ TheOldLobo/Assets/Scripts/GameOverScreen.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class GameOverScreen : MonoBehaviour
 {
     public Text pointsText;
+    public MoveController moveController;
+
     public void Setup()
     {
         gameObject.SetActive(true);
-
+        moveController.StopMovement();
     }
 
 }
-
