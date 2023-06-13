04de6dbb42ae76db33570c95d52fb5331205bd50
Joloharo <joeljoanlopez@gmail.com>
2023-06-13 11:34:19 +0200

    Bullet time hecho

--- TheOldLobo/Assets/Scripts/Combat/BulletTimeController.cs
+++ TheOldLobo/Assets/Scripts/Combat/BulletTimeController.cs
@@ -10,5 +10,13 @@ public class BulletTimeController : MonoBehaviour
     [SerializeField] float _TotalSeconds = 3;
     [SerializeField] InputActionReference _BulletTime;
 
-    void OnBulletTime()
+    void Update()
+    {
+        if (_BulletTime.action.IsInProgress() && _TotalSeconds > 0)
+        {
+            Time.timeScale = _TimeFraction;
+            _TotalSeconds -= Time.deltaTime;
+        }
+        else Time.timeScale = 1;
+    }
 }
